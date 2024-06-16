85d7001f8411fe1f472dbe6a7c17e9aa3426d6cb
Hachi <82988679+pichayakorn@users.noreply.github.com>
2024-06-16 15:48:28 +0900

    feat(hint): add general & specific hint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import openai
 import random
 import os
+import re
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from pathlib import Path
@@ -46,6 +47,8 @@ CATEGORIES = [
 get_word_prompt_template = open(Path("prompts/get_word.txt"), "r").read()
 get_word_score_prompt_template = open(Path("prompts/get_word_score.txt"), "r").read()
 get_hint_prompt_template = open(Path("prompts/get_hint.txt"), "r").read()
+get_general_hint_prompt_template = open(Path("prompts/get_general_hint.txt"), "r").read()
+get_final_hint_prompt_template = open(Path("prompts/get_final_hint.txt"), "r").read()
 
 def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL):
     try:
@@ -61,14 +64,26 @@ def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MO
         print(f"Exception: {e}")
         return e
 
-def rand_word(word_list: list):
-  if isinstance(word_list, list) and len(word_list) == 20:
-      shuffled_list = word_list.copy()
-      random.shuffle(shuffled_list)
-      return shuffled_list[:10]
+def detect_multiple_placeholders(sentence):
+    # Define a pattern to match placeholders like ________
+    pattern = r'_+'
 
-  else:
-    raise ValueError("word_list must be a list of 20 words")
+    # Find all occurrences of the pattern in the sentence
+    matches = re.findall(pattern, sentence)
+
+    # Check the number of matches
+    if len(matches) > 1:
+        return True
+    else:
+        return False
+
+def rand_word(word_list: list):
+    if isinstance(word_list, list) and len(word_list) == 20:
+      # Use random.sample to pick 10 unique elements from word_list
+        selected_words = random.sample(word_list, 10)
+        return selected_words
+    else:
+        return ValueError("Input should be a list of exactly 20 words.")
 
 def to_final_json(word_list: list, score_list: list, hint_list: list):
   hints = [{"ID": idx + 1, "question": hint_list[idx]} for idx in range(len(hint_list))]
@@ -108,7 +123,7 @@ tools = [
         "type": "function",
         "function": {
             "name": "get_word_score",
-            "description": "Get words semantic similarity scores.",
+            "description": "Get ten word semantic similarity scores.",
             "parameters": {
                 "type": "object",
                 "properties": {
@@ -117,7 +132,7 @@ tools = [
                         "items": {
                             "type": "number"
                         },
-                        "description": "Python dictionary of word and semantic similarity score between 0 to 1",
+                        "description": "A list of ten semantic similarity scores between 0 to 1. 0 indicates that the two texts have no semantic similarity. This means that the meanings of the texts are completely different or unrelated. 1 indicates that the two texts are semantically identical. e",
                     }
                 },
                 "required": ["score_list"]
@@ -128,7 +143,7 @@ tools = [
         "type": "function",
         "function": {
             "name": "get_hint",
-            "description": "Get 4 hint questions that must be fill in the blank question type.",
+            "description": "Get two fill-in-the-blank questions where the missing word can be filled in anywhere in the sentence. The questions should be based on the group that contains the target word, so the correct answer can be any word in that group. Ensure that every word in the group can fill in the missing word and the other group cannot fill in the missing word.",
             "parameters": {
                 "type": "object",
                 "properties": {
@@ -137,10 +152,44 @@ tools = [
                         "items": {
                             "type": "string"
                         },
-                        "description": """4 hint questions list consist of fill in the blank question, the question that has an underscore symbol somewhere in the sentence that denotes the missing word. The word first word in the provided list must be the missing word for all 4 hint questions. The choices are picked from 5 of 10 in the word list. The sentences consist of around 10 words. The missing position must have only one place in the sentence. The element type is string e.g. ["1st hint-blank-question", "2nd hint-blank-question", "3rd hint-blank-question", "4th hint-blank-question"]""",
+                        "description": """Two fill-in-the-blank questions where the missing word can be filled in anywhere in the sentence. The missing position must have only one place in the sentence denote with four underscore symbols. The element type is string e.g. ["1st fill-in-the-blank question", "2nd fill-in-the-blank question"]""",
                     }
                 },
                 "required": ["hint_list"]
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_general_hint",
+            "description": "Get one general fill-in-the-blank question where the missing place is somewhere in the sentence. All words in the list is correct answer.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "general_hint": {
+                        "type": "string",
+                        "description": """General fill-in-the-blank question where the missing place is somewhere in the sentence. The missing position must have only one place in the sentence denote with four underscore symbols. The element type is string e.g. 'general-fill-in-the-blank question'""",
+                    }
+                },
+                "required": ["general_hint"]
+            }
+        }
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "get_final_hint",
+            "description": "Get one fill-in-the-blank question where the missing place is somewhere in the sentence. The correct answer can only be one word, and the rest will be wrong.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "final_hint": {
+                        "type": "string",
+                        "description": """One general fill-in-the-blank question where the missing place is somewhere in the sentence. The missing position must have only one place in the sentence denote with four underscore symbols. The element type is string e.g. 'specific-fill-in-the-blank question'""",
+                    }
+                },
+                "required": ["final_hint"]
             }
         }
     },
@@ -188,6 +237,10 @@ def generate_words():
     get_word_score_prompt = get_word_score_prompt_template.replace("<TARGET_WORD>", target_word)
     get_word_score_prompt = get_word_score_prompt.replace("<WORD_LIST>", combined_list)
 
+    print("-------------------")
+    print(word_list, ",", len(word_list), "words")
+    print("-------------------")
+
     messages = []
     messages.append({"role": "user", "content": get_word_score_prompt})
 
@@ -217,18 +270,56 @@ def generate_words():
             except Exception as e:
                 return jsonify({'error': 'Failed to parse the response', 'details': str(e)}), 500
 
-    get_hint_prompt = get_hint_prompt_template.replace("<TARGET_WORD>", target_word)
-    get_hint_prompt = get_hint_prompt.replace("<WORD_LIST>", combined_list)
+    get_general_hint_prompt = get_general_hint_prompt_template.replace("<WORD_LIST>", combined_list)
+    get_hint_prompt = get_hint_prompt_template.replace("<TARGET_WORD>", target_word).replace("<WORD_LIST>", combined_list)
+    get_final_hint_prompt = get_final_hint_prompt_template.replace("<TARGET_WORD>", target_word).replace("<INCORRECT_WORD_LIST>", ', '.join(word_list[1:]))
 
-    messages = []
-    messages.append({"role": "user", "content": get_hint_prompt})
+    print("-------------------")
+    print(get_general_hint_prompt)
+    print("-------------------")
+    print(get_hint_prompt)
+    print("-------------------")
+    print(get_final_hint_prompt)
+    print("-------------------")
+
+    # Get general hint from the model
+    regen = True
+    general_hint = None
+    while(regen):
+      if (regen):
+
+        messages = []
+        messages.append({"role": "user", "content": get_general_hint_prompt})
+        response = chat_completion_request(
+            messages=messages,
+            tools=tools,
+            tool_choice={"type": "function", "function": {"name": "get_general_hint"}}
+        )
+
+        regen = False
+        print(response.choices[0].message.tool_calls)
+
+        general_hint = None
+        try:
+            tool_calls = response.choices[0].message.tool_calls
+            if tool_calls:
+              tool_function_name = tool_calls[0].function.name
+              general_hint = eval(tool_calls[0].function.arguments)['general_hint']
+            #   if ("__" not in general_hint):
+            #     regen = True
+              if detect_multiple_placeholders(general_hint) or target_word in general_hint:
+                regen = True
+            
+        except Exception as e:
+            return jsonify({'error': 'Failed to parse the response', 'details': str(e)}), 500
 
     # Get hints from the model
     regen = True
     hint_list = None
     while(regen):
       if (regen):
-
+        messages = []
+        messages.append({"role": "user", "content": get_hint_prompt})
         response = chat_completion_request(
             messages=messages,
             tools=tools,
@@ -243,13 +334,61 @@ def generate_words():
             if tool_calls:
               tool_function_name = tool_calls[0].function.name
               hint_list = eval(tool_calls[0].function.arguments)['hint_list']
-            for i in range(len(hint_list)):
-              if ("_" not in hint_list[i]):
+            if len(hint_list) == 2:
+                for i in range(len(hint_list)):
+                    # if ("__" not in hint_list[i]):
+                    #     regen = True
+                    if detect_multiple_placeholders(hint_list[i]) or target_word in hint_list[i]:
+                        regen = True
+                        break
+            else:
                 regen = True
-                break
+                
+        except Exception as e:
+            return jsonify({'error': 'Failed to parse the response', 'details': str(e)}), 500
+
+    # Get final hint from the model
+    regen = True
+    final_hint = None
+    while(regen):
+      if (regen):
+
+        messages = []
+        messages.append({"role": "user", "content": get_final_hint_prompt})
+        response = chat_completion_request(
+            messages=messages,
+            tools=tools,
+            tool_choice={"type": "function", "function": {"name": "get_final_hint"}}
+        )
+
+        regen = False
+        print(response.choices[0].message.tool_calls)
+
+        final_hint = None
+        try:
+            tool_calls = response.choices[0].message.tool_calls
+            if tool_calls:
+              tool_function_name = tool_calls[0].function.name
+              final_hint = eval(tool_calls[0].function.arguments)['final_hint']
+            #   if ("__" not in final_hint):
+            #     regen = True
+              if detect_multiple_placeholders(final_hint) or target_word in final_hint:
+                regen = True
             
         except Exception as e:
             return jsonify({'error': 'Failed to parse the response', 'details': str(e)}), 500
+
+    hint_list.insert(0, general_hint)
+    hint_list.append(final_hint)
+    print("-------------------")
+    print(hint_list)
+    print("-------------------")
+    print("Hint list")
+    for i in range(len(hint_list)):
+        print("ID", i+1, ": ", hint_list[i])
+    print("Choices:")
+    print(word_list)
+
 
 
     return to_final_json(word_list, score_list, hint_list)
